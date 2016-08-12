32abdb1e786fc2c9375cb8e3bb6643d58c41c541
darthjee <darthjee@gmail.com>
2016-08-12 14:40:23 -0300

    Perform calculation with number

--- spec/lib/danica/function/product_spec.rb
+++ spec/lib/danica/function/product_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe Danica::Function::Product do
   let(:variables_number) { 4 }
   let(:variables) do
-    Array.new(variables_number).map.with_index do |_,i|
-      Danica::Variable.new(name: "X#{i}", value: i+1)
+    (1..variables_number).map do |i|
+      Danica::Variable.new(name: "X#{i}", value: i)
     end
   end
   let(:subject) do
@@ -14,6 +14,14 @@ describe Danica::Function::Product do
   describe 'to_f' do
     it do
       expect(subject.to_f).to eq(24)
+    end
+
+    context 'when one variable is a number' do
+      let(:variables) { (1..variables_number) }
+
+       it do
+        expect(subject.to_f).to eq(24)
+      end
     end
   end
 end
