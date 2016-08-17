3412e161065faa8de1bfb490eec4ba061c491d6c
darthjee <darthjee@gmail.com>
2016-08-16 22:21:17 -0300

    Refactor shared example

--- spec/support/shared_examples/function.rb
+++ spec/support/shared_examples/function.rb
@@ -49,35 +49,35 @@ shared_examples 'a function that knows how to write to tex' do |arguments|
   end
 
   describe 'to_tex' do
-    context 'when variables have no value' do
-      let(:variables) do
-        (1..4).map do |i|
-          Danica::Variable.new(name: "X#{i}")
-        end
+    let(:variables) do
+      (1..4).map do |i|
+        Danica::Variable.new(name: "X#{i}")
       end
+    end
 
+    context 'when variables have no value' do
       it 'outputs a latex format' do
         expect(subject.to_tex).to eq(tex_expected)
       end
+    end
 
-      context 'when some variables have values' do
-        let(:numeric_variables_index) { 1 }
-        before do
-          (0..numeric_variables_index).each do |i|
-            variables[i].value = numeric_variables[i]
-          end
+    context 'when some variables have values' do
+      let(:numeric_variables_index) { 1 }
+      before do
+        (0..numeric_variables_index).each do |i|
+          variables[i].value = numeric_variables[i]
         end
+      end
+
+      it 'outputs a latex format with colapsed numbers' do
+        expect(subject.to_tex).to eq(tex_integer_expected)
+      end
+
+      context 'when numeric variables sum is a float value' do
+        let(:numeric_variables_index) { 2 }
 
         it 'outputs a latex format with colapsed numbers' do
-          expect(subject.to_tex).to eq(tex_integer_expected)
-        end
-
-        context 'when numeric variables sum is a float value' do
-          let(:numeric_variables_index) { 2 }
-
-          it 'outputs a latex format with colapsed numbers' do
-            expect(subject.to_tex).to eq(tex_float_expected)
-          end
+          expect(subject.to_tex).to eq(tex_float_expected)
         end
       end
     end
