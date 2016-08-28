5f83c25d45d15580b13677b8eba8b14595f64df3
darthjee <darthjee@gmail.com>
2016-08-27 22:53:24 -0300

    Add sample function

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # danica
 A tool for math formulation on docs
+
+## How to use
+Add the gem to your project or just install the gem
+
+```
+gem 'danica'
+```
+
+```console
+bundle install danica
+```
+
+Now you can use in your project
+
+
+```ruby
+class MyFunction < Danica::Function
+  def to_f
+    #implement to float method
+  end
+
+  def to_tex
+    #implement to tex method
+  end
+end
+```
+
+
+### Sample
+
+```ruby
+class Danica::Function
+  class Spatial < Danica::Function
+    attr_accessor :time, :acceleration, :initial_space, :initial_velocity
+    delegate :to_tex, to: :sum
+
+    private
+
+    def sum
+      @sum ||= Sum.new(variables: parcels)
+    end
+
+    def parcels
+      [
+        initial_space,
+        spatial_velocity,
+        spatial_acceleration
+      ]
+    end
+
+    def spatial_velocity
+      Product.new(variables: [ initial_velocity, time ])
+    end
+
+    def spatial_acceleration
+      Division.new(numerator: Product.new(variables: [ acceleration, time, time ]), denominator: 2)
+    end
+  end
+end
+```
+
+returns
+```string
+S_0+V_0\cdott+\frac{a\cdott\cdott}{2}
+```
+
---
+++ spec/lib/danica/function_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe Danica::Function do
+  class Danica::Function
+    class Spatial < Danica::Function
+      attr_accessor :time, :acceleration, :initial_space, :initial_velocity
+      delegate :to_tex, to: :sum
+
+      private
+
+      def sum
+        @sum ||= Sum.new(variables: parcels)
+      end
+
+      def parcels
+        [
+          initial_space,
+          spatial_velocity,
+          spatial_acceleration
+        ]
+      end
+
+      def spatial_velocity
+        Product.new(variables: [ initial_velocity, time ])
+      end
+
+      def spatial_acceleration
+        Division.new(numerator: Product.new(variables: [ acceleration, time, time ]), denominator: 2)
+      end
+    end
+  end
+
+  describe '#to_tex' do
+    context 'when creating the spatial function for constantly accelerated movement' do
+      let(:variables) do
+        {
+          time: Danica::Variable.new(name: :t),
+          acceleration: Danica::Variable.new(name: :a),
+          initial_space: Danica::Variable.new(name: :S0, latex: 'S_0'),
+          initial_velocity: Danica::Variable.new(name: :V0, latex: 'V_0')
+        }
+      end
+
+      let(:subject) { described_class::Spatial.new(variables) }
+      let(:expected) { 'S_0+V_0\cdott+\frac{a\cdott\cdott}{2}'  }
+
+      it 'return the latex format CAM' do
+        expect(subject.to_tex).to eq(expected)
+      end
+    end
+  end
+end
