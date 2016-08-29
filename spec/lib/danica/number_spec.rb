29f902bd553302e2df97965cbdb72cd6e6383f28
darthjee <darthjee@gmail.com>
2016-08-28 23:59:58 -0300

    Add number spec

---
+++ spec/lib/danica/number_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe Danica::Number do
+  let(:value) { 10 }
+  let(:subject) { described_class.new(value) }
+
+  describe '#to_f' do
+    it 'returns the float of value' do
+      expect(subject.to_f).to eq(10)
+    end
+
+    it { expect(subject.to_f).to be_a(Float) }
+  end
+
+  describe '#to_tex' do
+    context 'when value should be integer' do
+      let(:value) { 10.0 }
+
+      it 'returns the value integer string' do
+        expect(subject.to_tex).to eq('10')
+      end
+    end
+
+    context 'when value should be integer' do
+      let(:value) { 10.5 }
+
+      it 'returns the value integer string' do
+        expect(subject.to_tex).to eq('10.5')
+      end
+    end
+  end
+end
