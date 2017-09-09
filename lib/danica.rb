8fc4cf8e253593efc9e0ab71e90ecee01260b3cd
darthjee <darthjee@gmail.com>
2017-09-09 19:31:03 +0200

    Use autoload instead of require

--- lib/danica.rb
+++ lib/danica.rb
@@ -1,12 +1,21 @@
 require 'active_model'
 
 module Danica
-  require 'danica/common'
-  require 'danica/number'
-  require 'danica/variable'
-  require 'danica/operator'
-  require 'danica/function'
-  require 'danica/exception'
-  require 'danica/constant'
+  autoload :Common,      'danica/common'
+  autoload :Number,      'danica/number'
+  autoload :Variable,    'danica/variable'
+  autoload :Operator,    'danica/operator'
+  autoload :Function,    'danica/function'
+  autoload :Exception,   'danica/exception'
+  autoload :Constant,    'danica/constant'
+
+  autoload :Product,     'danica/product'
+  autoload :Sum,         'danica/sum'
+  autoload :Division,    'danica/division'
+  autoload :Power,       'danica/power'
+  autoload :SquareRoot,  'danica/square_root'
+  autoload :Exponential, 'danica/exponential'
+  autoload :Sin,         'danica/sin'
+  autoload :Cos,         'danica/cos'
 end
 
