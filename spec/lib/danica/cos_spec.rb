b33e22fb71f455aaee8c65fc2e65e27eaa35cc1e
darthjee <darthjee@gmail.com>
2017-09-14 18:02:35 +0200

    Correct cos spec

--- spec/lib/danica/cos_spec.rb
+++ spec/lib/danica/cos_spec.rb
@@ -9,7 +9,8 @@ describe Danica::Cos do
   it_behaves_like 'a operator with a single input value', {
     variable_value: Math::PI,
     expected_number: -1.0,
-    expected_number_text: '-1',
+    expected_number_tex: 'cos(3.141592653589793)',
+    expected_number_gnu: 'cos(3.141592653589793)',
     expected_tex: 'cos(X)',
     expected_gnu: 'cos(X)'
   }
