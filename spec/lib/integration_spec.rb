f3edbb76cae1d7f3b43d64275b9c74c280756d3d
darthjee <darthjee@gmail.com>
2017-09-18 09:32:31 +0200

    Better spec description

--- spec/lib/integration_spec.rb
+++ spec/lib/integration_spec.rb
@@ -2,17 +2,19 @@ require 'spec_helper'
 
 describe 'integration' do
   describe '#to_gnu' do
-    describe 'a sum with negative numbers' do
-      subject do
-        Danica::Sum.new(Danica::Negative.new(1), 2, Danica::Negative.new(3), 4)
-      end
+    describe 'a sum' do
+      describe 'with negative numbers' do
+        subject do
+          Danica::Sum.new(Danica::Negative.new(1), 2, Danica::Negative.new(3), 4)
+        end
 
-      it 'returns the correct string' do
-        expect(subject.to_gnu).to eq('-1 + 2 -3 + 4')
+        it 'returns the correct string' do
+          expect(subject.to_gnu).to eq('-1 + 2 -3 + 4')
+        end
       end
     end
 
-    describe 'a product of' do
+    describe 'a product' do
       describe 'of number and sum' do
         subject do
           Danica::Product.new(
@@ -25,7 +27,7 @@ describe 'integration' do
         end
       end
 
-      describe 'sums' do
+      describe 'of sums' do
         subject do
           Danica::Product.new(
             Danica::Sum.new(1,2),
