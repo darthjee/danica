864023ceb2000389497995f6b41271135e502e35
darthjee <darthjee@gmail.com>
2017-09-19 14:18:24 +0200

    commit
    Split tests

--- spec/lib/integration_spec.rb
+++ spec/lib/integration_spec.rb
@@ -1,55 +1,61 @@
 require 'spec_helper'
 
 describe 'integration' do
-  describe '#to_gnu' do
-    describe 'power' do
-      describe 'of sums' do
-        subject do
-          Danica::Power.new(
-            Danica::Sum.new(3, 4),
-            Danica::Sum.new(5, 6)
-          )
-        end
+  describe 'power' do
+    describe 'of sums' do
+      subject do
+        Danica::Power.new(
+          Danica::Sum.new(3, 4),
+          Danica::Sum.new(5, 6)
+        )
+      end
 
+      describe '#to_gnu' do
         it 'returns the correct string' do
           expect(subject.to_gnu).to eq('(3 + 4)**(5 + 6)')
         end
       end
     end
+  end
 
-    describe 'a sum' do
-      describe 'with negative numbers' do
-        subject do
-          Danica::Sum.new(Danica::Negative.new(1), 2, Danica::Negative.new(3), 4)
-        end
+  describe 'a sum' do
+    describe 'with negative numbers' do
+      subject do
+        Danica::Sum.new(Danica::Negative.new(1), 2, Danica::Negative.new(3), 4)
+      end
 
+      describe '#to_gnu' do
         it 'returns the correct string' do
           expect(subject.to_gnu).to eq('-1 + 2 -3 + 4')
         end
       end
     end
+  end
 
-    describe 'a product' do
-      describe 'of number and sum' do
-        subject do
-          Danica::Product.new(
-            3, Danica::Sum.new(2, 4)
-          )
-        end
+  describe 'a product' do
+    describe 'of number and sum' do
+      subject do
+        Danica::Product.new(
+          3, Danica::Sum.new(2, 4)
+        )
+      end
 
+      describe '#to_gnu' do
         it 'returns the correct string' do
           expect(subject.to_gnu).to eq('3 * (2 + 4)')
         end
       end
+    end
 
-      describe 'of sums' do
-        subject do
-          Danica::Product.new(
-            Danica::Sum.new(1,2),
-            Danica::Sum.new(3,4)
-          )
-        end
+    describe 'of sums' do
+      subject do
+        Danica::Product.new(
+          Danica::Sum.new(1,2),
+          Danica::Sum.new(3,4)
+        )
+      end
 
+      describe '#to_gnu' do
         it 'returns the correct string' do
           expect(subject.to_gnu).to eq('(1 + 2) * (3 + 4)')
         end
