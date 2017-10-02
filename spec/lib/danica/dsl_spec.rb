6c74c47f7419ceaf47bddeb1644abb0d3ac86576
darthjee <darthjee@gmail.com>
2017-10-02 10:44:17 +0200

    Extract more shared examples

--- spec/lib/danica/dsl_spec.rb
+++ spec/lib/danica/dsl_spec.rb
@@ -1,3 +1,13 @@
+shared_context 'a class with alias to a clazz' do |aliaz, clazz, *variables|
+  it do
+    expect(subject).to respond_to(aliaz)
+  end
+
+  it "has an alias #{aliaz} for #{clazz}" do
+    expect(subject.public_send(aliaz, *variables)).to eq(clazz.new(*variables))
+  end
+end
+
 shared_context 'a class with mapped dsl' do
   mapping = {
     sum: Danica::Sum,
@@ -8,16 +18,8 @@ shared_context 'a class with mapped dsl' do
     power: Danica::Power
   }
 
-  let(:variables) { [2, 3] }
-
   mapping.each do |aliaz, clazz|
-    it do
-      expect(subject).to respond_to(aliaz)
-    end
-
-    it "has an alias #{aliaz} for #{clazz}" do
-      expect(subject.public_send(aliaz, *variables)).to eq(clazz.new(*variables))
-    end
+    it_behaves_like 'a class with alias to a clazz', aliaz, clazz, 2, 3
   end
 end
 
