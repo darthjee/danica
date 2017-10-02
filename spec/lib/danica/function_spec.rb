7af6d42ae7dcdd620c2bd498296be96cee7cab05
darthjee <darthjee@gmail.com>
2017-10-02 23:22:11 +0200

    Test block definition

--- spec/lib/danica/function_spec.rb
+++ spec/lib/danica/function_spec.rb
@@ -54,6 +54,18 @@ describe Danica::Function do
       it 'has the defined variables' do
         expect(function.variables_hash).to eq(x: nil)
       end
+
+      context 'when calling to_tex' do
+        it 'build function from block' do
+          expect(function.to_tex).to eq('x^{2}')
+        end
+      end
+
+      context 'when calling to_gnu' do
+        it 'build function from block' do
+          expect(function.to_gnu).to eq('x**(2)')
+        end
+      end
     end
 
     context 'when using a class that inherits from another class' do
@@ -65,6 +77,18 @@ describe Danica::Function do
 
       it 'has the defined variables' do
         expect(function.variables_hash).to eq(x: nil, y: nil)
+      end
+
+      context 'when calling to_tex' do
+        it 'build function from block' do
+          expect(function.to_tex).to eq('x^{2} -y^{2}')
+        end
+      end
+
+      context 'when calling to_gnu' do
+        it 'build function from block' do
+          expect(function.to_gnu).to eq('x**(2) -y**(2)')
+        end
       end
     end
   end
--- spec/support/models/functions/hyperbole.rb
+++ spec/support/models/functions/hyperbole.rb
@@ -1,10 +1,14 @@
 module Danica
-  class Hyperbole < Function.build(:x)
+  class Hyperbole < Function.build(:x) { power(x, 2) }
   end
 end
 
 module Danica
   class SaddleHyperbole < Hyperbole
     variables :y
+
+    def function_block
+      super - power(y, 2)
+    end
   end
 end
