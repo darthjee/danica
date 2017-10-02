ecd56dd7c6521fa10469cb4816dc3c5952f5cf87
darthjee <darthjee@gmail.com>
2017-10-02 10:24:05 +0200

    Do not use send

--- lib/danica/dsl.rb
+++ lib/danica/dsl.rb
@@ -2,7 +2,7 @@ module Danica
   module DSL
     def self.register(method, clazz=nil)
       clazz = "Danica::#{method.to_s.camelize}".constantize unless clazz
-      DSL.send(:define_method, method) do |*args|
+      define_method method do |*args|
         clazz.new(*args)
       end
     end
