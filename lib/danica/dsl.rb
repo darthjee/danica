275167447edcccd54c985cb03910239ba09c00c9
darthjee <darthjee@gmail.com>
2017-10-02 09:52:50 +0200

    Use registry approach

--- lib/danica/dsl.rb
+++ lib/danica/dsl.rb
@@ -1,16 +1,13 @@
 module Danica
   module DSL
-    def sum(*args)
-      Sum.new(*args)
-    end
-
-    def division(*args)
-      Division.new(*args)
-    end
-
-    def product(*args)
-      Product.new(*args)
+    def self.register(method, clazz)
+      DSL.send(:define_method, method) do |*args|
+        clazz.new(*args)
+      end
     end
   end
-end
 
+  DSL.register(:sum, Sum)
+  DSL.register(:product, Product)
+  DSL.register(:division, Division)
+end
