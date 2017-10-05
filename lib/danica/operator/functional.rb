e1344ec6856b4c452e32b40f953256c1cac43412
darthjee <darthjee@gmail.com>
2017-10-05 22:43:08 +0200

    Use eval instead of define method

--- lib/danica/operator/functional.rb
+++ lib/danica/operator/functional.rb
@@ -5,17 +5,19 @@ module Danica
 
     def self.build(operator:, tex:, gnu:)
       Class.new(self) do
-        define_method :to_f do
-          Math.public_send(operator, value.to_f)
-        end
+        module_eval(%Q(
+          def to_f
+            Math.#{operator}(value.to_f)
+          end
 
-        define_method :to_tex do
-          tex.gsub(':value:', value.to_tex)
-        end
+          def to_tex
+            '#{tex.gsub(':value:', "' + value.to_tex + '")}'
+          end
 
-        define_method :to_gnu do
-          gnu.gsub(':value:', value.to_gnu)
-        end
+          def to_gnu
+            '#{gnu.gsub(':value:', "' + value.to_gnu + '")}'
+          end
+        ))
       end
     end
   end
