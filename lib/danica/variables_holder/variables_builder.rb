0c8ec63fb646f8b9e71136d2c43dfeb1279bea64
darthjee <darthjee@gmail.com>
2017-10-09 21:51:32 +0200

    Recode variable wrapper to return values from class defaults

--- lib/danica/variables_holder/variables_builder.rb
+++ lib/danica/variables_holder/variables_builder.rb
@@ -1,5 +1,8 @@
 module Danica::VariablesHolder
   class VariablesBuilder
+    include Danica::DSL
+    include Danica::Wrapper
+
     attr_reader :instance, :attr_names
 
     def initialize(attr_names, instance)
@@ -12,14 +15,14 @@ module Danica::VariablesHolder
 
       attr_names.each do |name|
         add_setter(name)
-        add_reader(name, name)
-        instance.variables_names << name
+        add_reader(name)
+        instance.variables_hash[name.to_sym] = wrap_value(name)
       end
 
       names_hash.each do |name, default|
         add_setter(name)
-        add_reader(name, default)
-        instance.variables_names << name
+        add_reader(name)
+        instance.variables_hash[name.to_sym] = wrap_value(default)
       end
     end
 
@@ -28,13 +31,12 @@ module Danica::VariablesHolder
     def add_setter(name)
       instance.send(:define_method, "#{name}=") do |value|
         variables_hash[name.to_sym] = wrap_value(value)
-        @variables = variables_hash.values
       end
     end
 
-    def add_reader(name, default)
+    def add_reader(name)
       instance.send(:define_method, name) do
-        variables_hash[name.to_sym] ||= wrap_value(default)
+        variables_hash[name.to_sym]
       end
     end
   end
--- spec/lib/danica/variables_holder_spec.rb
+++ spec/lib/danica/variables_holder_spec.rb
@@ -26,14 +26,36 @@ describe Danica::VariablesHolder do
     end
   end
 
+  describe '#variables=' do
+    before do
+      subject.variables = variables
+    end
+
+    context 'when setting the variables using a list' do
+      let(:variables) { [1, 2, 3] }
+
+      it 'changes the values of the variables' do
+        expect(subject.x).to eq(Danica::Wrapper::Number.new(1))
+      end
+    end
+
+    context 'when setting the variales using a hash' do
+      let(:variables) { { x: 1, y: 2, z: 3 } }
+
+      it 'changes the values of the variables' do
+        expect(subject.x).to eq(Danica::Wrapper::Number.new(1))
+      end
+    end
+  end
+
   describe '#variables' do
     context 'when instance has no variables defined' do
       it do
         expect(subject.variables).not_to be_empty
       end
 
-      it 'returns only nil values' do
-        expect(subject.variables.compact).to be_empty
+      it 'returns the base variables set on the class' do
+        expect(subject.variables).to eq(clazz.variables_hash.values)
       end
     end
 
@@ -42,18 +64,24 @@ describe Danica::VariablesHolder do
         subject.y = 1
       end
 
-      it 'returns not only nil values' do
-        expect(subject.variables).to eq([
-          nil, Danica::Wrapper::Number.new(1), nil
-        ])
+      it 'returns the default variables and the new set one' do
+        expect(subject.variables).to eq(clazz.variables_hash.merge(
+          y: Danica::Wrapper::Number.new(1)
+        ).values)
+      end
+
+      it 'does not change the class variables' do
+        expect do
+          subject.z = 2
+        end.not_to change { clazz.variables_hash }
       end
     end
   end
 
   describe '#variables_hash' do
     context 'when instance has no variables defined' do
-      it 'returns the variables map with empty values' do
-        expect(subject.variables_hash).to eq({x: nil, y: nil, z: nil})
+      it 'returns the variables map with default values' do
+        expect(subject.variables_hash).to eq(clazz.variables_hash)
       end
     end
 
@@ -62,20 +90,18 @@ describe Danica::VariablesHolder do
         subject.y = 1
       end
 
-      it 'returns not only nil values' do
-        expect(subject.variables_hash).to eq({
-          x: nil,
-          y: Danica::Wrapper::Number.new(1),
-          z: nil
-        })
+      it 'returns the default hash with the set value' do
+        expect(subject.variables_hash).to eq(clazz.variables_hash.merge(
+          y: Danica::Wrapper::Number.new(1)
+        ))
       end
     end
   end
   
   describe '#variables_value_hash' do
     context 'when instance has no variables defined' do
-      it 'returns a nil valued hash' do
-        expect(subject.variables_value_hash).to eq({x: nil, y: nil, z: nil})
+      it 'returns the default value' do
+        expect(subject.variables_value_hash).to eq({x: nil, y: nil, z: 10})
       end
     end
 
@@ -85,7 +111,7 @@ describe Danica::VariablesHolder do
       end
 
       it 'returns the values' do
-        expect(subject.variables_value_hash).to eq({x: nil, y: 1, z: nil})
+        expect(subject.variables_value_hash).to eq({x: nil, y: 1, z: 10})
       end
     end
   end
