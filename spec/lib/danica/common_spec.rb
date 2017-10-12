4d654e96915db4bdcc449921d0d41d107d9b4d64
darthjee <darthjee@gmail.com>
2017-10-12 18:47:13 +0200

    Add spec for Common#to

--- spec/lib/danica/common_spec.rb
+++ spec/lib/danica/common_spec.rb
@@ -2,6 +2,14 @@ require 'spec_helper'
 
 class Danica::Common::Dummy
   include Danica::Common
+
+  def to_tex
+    'tex'
+  end
+
+  def to_gnu
+    'gnu'
+  end
 end
 
 describe Danica::Common do
@@ -13,6 +21,38 @@ describe Danica::Common do
       expect do
         subject.to_f
       end.to raise_error(Danica::Exception::NotImplemented)
+    end
+  end
+
+  describe '#to' do
+    context 'when defining to_tex and to_gnu' do
+      context 'when requesting :tex' do
+        it 'has a string for latex' do
+          expect(subject.to(:tex)).to eq('tex')
+        end
+      end
+      context "when requesting 'tex'" do
+        it 'has a string for latex' do
+          expect(subject.to('tex')).to eq('tex')
+        end
+      end
+      context 'when requesting :gnu' do
+        it 'has a string for gnu' do
+          expect(subject.to(:gnu)).to eq('gnu')
+        end
+      end
+      context "when requesting 'gnu'" do
+        it 'has a string for gnu' do
+          expect(subject.to('gnu')).to eq('gnu')
+        end
+      end
+      context "when requesting wrong format" do
+        it do
+          expect do
+            subject.to('format')
+          end.to raise_error(Danica::Exception::FormatNotFound)
+        end
+      end
     end
   end
 end
