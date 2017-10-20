9315dac1f656884dba95755813adefbe5dccf3ea
darthjee <darthjee@gmail.com>
2017-10-20 09:33:16 +0200

    Delegate more methods to content

--- lib/danica/wrapper/container.rb
+++ lib/danica/wrapper/container.rb
@@ -4,7 +4,7 @@ module Danica
     attr_accessor :content
 
     delegate :to_f, :contentd?, :to, :to_tex, :to_gnu, :priority, :grouped?,
-             :signaled?, :constant?, :valued?, to: :content
+             :signaled?, :constant?, :valued?, :*, :+, :-, :/, :**, to: :content
 
     default_value :container?, true
 
