38d240c5612d441a901d72ad8fbc3678a89ff2ec
darthjee <darthjee@gmail.com>
2017-12-15 13:43:59 +0100

    Refactoring calculator

--- lib/danica/variables_holder/calculator.rb
+++ lib/danica/variables_holder/calculator.rb
@@ -3,8 +3,6 @@ module Danica
     class Calculator
       attr_accessor :object, :args
 
-      delegate :variables_value_hash, to: :object
-
       def initialize(object, *args)
         @object = object
         @args = args
@@ -18,24 +16,25 @@ module Danica
 
       private
 
+      def vars_map
+        @vars_map ||= variables_value_hash.tap do |map|
+          vars = args.dup
+          map.merge! vars.extract_options!
+          non_valued_keys =  map.select { |_, value| !valued?(value) }.keys
+          map.merge! vars.as_hash(non_valued_keys)
+        end
+      end
+
       def all_valued?
         vars_map.values.all? { |v| valued?(v) }
       end
 
-      def vars_map
-        @vars_map ||= variables_value_hash.dup.tap do |map|
-          vars = args.dup
-          map.merge! vars.extract_options!
-          map.each do |name, value|
-            unless valued?(value)
-              map[name] = vars.shift
-            end
-          end
-        end
-      end
-
       def valued?(value)
         value.is_a?(Integer) || value.try(:valued?)
+      end
+
+      def variables_value_hash
+        @variables_value_hash ||= object.variables_value_hash.dup
       end
 
       def clazz
