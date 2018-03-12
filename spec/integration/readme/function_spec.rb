0e0ea5ada807e61818ce74baf388eb92f57c5666
darthjee <darthjee@gmail.com>
2018-03-12 18:41:57 +0100

    Add README over Function.for

--- spec/integration/readme/function_spec.rb
+++ spec/integration/readme/function_spec.rb
@@ -72,3 +72,11 @@ describe Danica::Function::MyFunction do
     end
   end
 end
+
+describe Danica::Function::QuadraticSum do
+  describe '#to_tex' do
+    it 'creates a funcion out of an expression' do
+      expect(subject.to_tex).to eq("f(x, y) = \\left(x + y\\right)^{2}")
+    end
+  end
+end
---
+++ spec/support/models/function/quadratic_sum.rb
@@ -0,0 +1,8 @@
+require_relative '../expression/quadratic_sum'
+
+module Danica
+  class Function
+    class QuadraticSum < Function.for(Expression::QuadraticSum)
+    end
+  end
+end
