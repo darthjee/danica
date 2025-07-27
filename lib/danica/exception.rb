c506b968b559a9f15ecc353b2aa96c728144acfe
darthjee <darthjee@gmail.com>
2025-07-27 12:49:59 -0300

    Fix Layout/EmptyLineBetweenDefs

--- lib/danica/exception.rb
+++ lib/danica/exception.rb
@@ -4,6 +4,7 @@ class Danica::Exception < RuntimeError
   class NotDefined < self; end
   class FormattedNotFound < self; end
   class NotImplemented < self; end
+
   class InvalidInput < self
     attr_reader :value
     def initialize(value)
